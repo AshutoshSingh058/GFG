2b2a7c9372052f79b18d67393cc4bb8da65a108d
AshutoshSingh058 <147975384+AshutoshSingh058@users.noreply.github.com>
2025-01-30 07:55:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Set the rightmost unset bit/set-the-rightmost-unset-bit.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBit(int n) {
+        // Write Your Code here
+        int i=0;
+        while(true){
+            if((n&(1<<i)) ==0 ){
+                n=n| (1<<i);
+                break;
+            }
+            i++;
+        }
+        return n;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        Solution ob;
+        int ans = ob.setBit(N);
+        cout << ans << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
